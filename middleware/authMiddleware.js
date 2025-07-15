27be26af4ae94edac5f0b0177bdaab991d6c78d5
Suraksha-S <surakshams553@gmail.com>
2025-07-15 22:33:32 +0530

    verifying access token

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -11,7 +11,7 @@ const protect = (req, res, next)=>{
     const token = authHeader.split(' ')[1];
 
     try {
-        const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRETE);
+        const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
         req.user = decoded;
         next();
         
